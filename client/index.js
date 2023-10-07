8136265d463abbd961472c5f5377b3e9a79e9af9
Dominic Kenny <dominicjkenny@gmail.com>
2023-10-07 14:25:42 -0400

    Added browser router

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './store';
 import styles from './scss/index.scss';
@@ -8,8 +9,10 @@ import styles from './scss/index.scss';
 const root = createRoot(document.getElementById('app'));
 
 root.render(
-  <Provider store="store">
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store="store">
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
 
